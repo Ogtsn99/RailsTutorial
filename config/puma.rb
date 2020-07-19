ce7d6b9e772832f1c04a13a7c57f2f74ec5b3502
Ogtsn99 <fantasista4123@icloud.com>
2020-07-19 20:34:40 +0900

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -30,7 +30,18 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
 
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
