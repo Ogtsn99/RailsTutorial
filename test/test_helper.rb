73f2fc73686a0d407223e1a026ec9113f2521cfe
Ogtsn99 <fantasista4123@icloud.com>
2020-07-25 18:20:12 +0900

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,7 +20,9 @@ end
 class ActionDispatch::IntegrationTest
 
   #テストユーザーとしてログインする
-  def log_in_as(user, password: 'password', remember_me:'1')
-    post login_path, params:{session:{email: user.email, password: password, remember_me: remember_me}}
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
